6a67d7a0ff491781afc1a5eeda758210579b0ec9
ofdun <elik394@mail.ru>
2023-05-24 00:21:17 +0300

    async chrome storage to check if slider status

--- developing/background.js
+++ developing/background.js
@@ -1,21 +1,29 @@
 window.onload = function () {
-    const interval = setInterval(function() {
-        if (window.location.href === 'https://www.youtube.com/feed/subscriptions') {
-            clearInterval(interval);
-            console.log('Script started!');
-            const videos = document.getElementsByTagName("ytd-grid-video-renderer");
-            Array.from(videos).forEach(function (item) {
-                const thumbnail_html = item.getElementsByTagName('a')[0];
-                const videos_href = thumbnail_html.href;
-                if (check_for_shorts(videos_href)) {
-                    item.remove();
-                }
-            })
-        }
-      }, 1000);
-
     function check_for_shorts(link) {
         return link.slice(24)[0] === 's';
     }
-    
+
+    function app_working(callback) {
+        chrome.storage.sync.get(['checkbox'], (items) => {
+            callback(items['checkbox'] === 'true');
+        })
+    }
+
+    app_working(function (result) {
+        if (!result) return;
+        const interval = setInterval(function () {
+            if (window.location.href === 'https://www.youtube.com/feed/subscriptions') {
+                clearInterval(interval);
+                console.log('Script started!');
+                const videos = document.getElementsByTagName("ytd-grid-video-renderer");
+                Array.from(videos).forEach(function (item) {
+                    const thumbnail_html = item.getElementsByTagName('a')[0];
+                    const videos_href = thumbnail_html.href;
+                    if (check_for_shorts(videos_href)) {
+                        item.remove();
+                    }
+                })
+            }
+        }, 1000);
+    })
 }
