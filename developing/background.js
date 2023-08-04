82af28e5d42f11b67388ff231d4edbc4bc8eb8fb
ofdun <elik394@mail.ru>
2023-08-04 23:33:15 +0300

    adjusted for newer youtube update

--- developing/background.js
+++ developing/background.js
@@ -1,8 +1,4 @@
 window.onload = function () {
-    function check_for_shorts(link) {
-        return link.slice(24)[0] === 's';
-    }
-
     function app_working(callback) {
         chrome.storage.sync.get(['checkbox'], (items) => {
             callback(items['checkbox'] === 'true');
@@ -15,11 +11,10 @@ window.onload = function () {
             if (window.location.href === 'https://www.youtube.com/feed/subscriptions') {
                 clearInterval(interval);
                 console.log('Script started!');
-                const videos = document.getElementsByTagName("ytd-grid-video-renderer");
+                const videos = document.getElementsByTagName("ytd-rich-section-renderer");
                 Array.from(videos).forEach(function (item) {
-                    const thumbnail_html = item.getElementsByTagName('a')[0];
-                    const videos_href = thumbnail_html.href;
-                    if (check_for_shorts(videos_href)) {
+                    const shorts = item.getElementsByTagName('ytd-rich-shelf-renderer');
+                    if (shorts !== undefined) {
                         item.remove();
                     }
                 })
